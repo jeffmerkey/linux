7779bc5da3c6e70b4a627fb59229464285944b64
Jeff Merkey <jeffmerkey@gmail.com>
2015-11-28 22:13:10 -0700

    Add MDB Debugger to kernel v3.11

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -983,6 +983,7 @@ void rcu_cpu_stall_reset(void)
 	for_each_rcu_flavor(rsp)
 		rsp->jiffies_stall = jiffies + ULONG_MAX / 2;
 }
+EXPORT_SYMBOL(rcu_cpu_stall_reset);
 
 /*
  * Initialize the specified rcu_data structure's callback list to empty.
