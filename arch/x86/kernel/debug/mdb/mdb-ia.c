001204e5868562057957f16b9c5c8ef2fd90dd82
Jeff Merkey <jeffmerkey@gmail.com>
2015-12-05 00:02:02 -0700

    clear debugreg6 DR_TRAP status

--- arch/x86/kernel/debug/mdb/mdb-ia.c
+++ arch/x86/kernel/debug/mdb/mdb-ia.c
@@ -6545,6 +6545,8 @@ MDBLoop:;
     switch (Exception)
     {
           case 1:/* int 1 debug exception */
+            if (current->thread.debugreg6 & DR_STEP)
+		current->thread.debugreg6 &= ~DR_STEP;
 	    if (per_cpu(BreakMask, processor))
 	    {
 	       stackFrame->tSystemFlags &= ~SINGLE_STEP;
