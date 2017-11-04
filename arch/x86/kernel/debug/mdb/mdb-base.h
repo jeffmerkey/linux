2fee115c210119e10c7a22a34865ecef830af2bb
Jeffrey Merkey <jeffmerkey@gmail.com>
2017-11-03 18:22:20 -0600

    fix checkpatch.pl errors and warnings
    Signed-off-by: Jeffrey Merkey <jeffmerkey@gmail.com>

--- arch/x86/kernel/debug/mdb/mdb-base.h
+++ arch/x86/kernel/debug/mdb/mdb-base.h
@@ -25,7 +25,7 @@ extern unsigned long line_info_toggle;
 extern unsigned char *last_link;
 
 int bt_stack(struct task_struct *task, struct pt_regs *regs,
-	      unsigned long *stack);
+	     unsigned long *stack);
 void machine_emergency_restart(void);
 void dump_os_symbol_table_match(unsigned char *match);
 void clear_screen(void);
